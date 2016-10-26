cf5289ade804d41cf648cb4aad0d426ea748f2ea
lawless-m <maht0x0r@xsmail.com>
2016-10-26 11:43:30 -0400

    shedding

--- main.cc
+++ main.cc
@@ -262,7 +262,7 @@ color(const ray& r, hitable *world, int depth) {
         vec3 attenuation;
         vec3 emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
         if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) 
-             return emitted + attenuation*color(scattered, world, depth+1);
+             return emitted + attenuation * color(scattered, world, depth+1);
         else 
             return emitted;
     }
